a99ffe39212ed8f498e0f8058c7b7412ad9f8dec
Jurgen Barbier <Jurgen@Jurgens-MacBook-Pro.local>
2015-04-29 20:20:35 +0200

    worked on the profile pages

--- src/controllers/AngAPICtrl.js
+++ src/controllers/AngAPICtrl.js
@@ -1,7 +1,24 @@
 app.controller("AngAPIController", function($scope, APIservice, $mdSidenav, muppetService, $routeParams){
            
     //scope is wat de view allemaal kent
-    
+     $scope.user = {
+        email: 'Jurgenbarbier@gmail.com',
+        firstName: 'Jurgen',
+        lastName: 'Barbier' ,
+        address: 'Heidestraat 88' ,
+        city: 'Schelle' ,
+        biography: 'I created this app',
+        postalCode : '2627',
+        hotelmin: "1",
+        hotelmax: "5",
+        departurepoint:"BRU",
+        food:"7",
+        Travel:{
+            airtravel:true,
+            cartravel:true,
+            traintravel:false,
+        }
+    };
     $scope.activitycategories = [
         
     ];
@@ -13,6 +30,27 @@ app.controller("AngAPIController", function($scope, APIservice, $mdSidenav, mupp
     $scope.destinations = [
         
     ];
+    $scope.selectUsertab = selectUsertab;
+    $scope.selected = null;
+    $scope.usertabs = [{
+      id:'1',
+      name: 'Basic Info',
+      iconurl: './assets/images/icons/basicinfo.svg'
+  }, {
+      id:'2',
+      name: 'Travel info',
+      iconurl: './assets/images/icons/transport.svg'
+  }, {
+      id:'3',
+      name: 'entertainement',
+      iconurl: './assets/images/icons/activities.svg'
+  }];
+    $scope.selected = $scope.usertabs[0];
+    
+    function selectUsertab(muppet) {
+    $scope.selected = angular.isNumber(muppet) ? $scope.usertabs[muppet] : muppet;
+    $scope.toggleSidenav('left');
+  }
     
     $scope.toggleSidenav = toggleSidenav;
     function toggleSidenav(name) {
